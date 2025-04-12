83d8929f9aaacd1b616fc0f3f0c70d0397210ed0
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-04-12 18:41:25 -0300

    chore: updates gateway type return

--- src/gateway/QuoteGateway.ts
+++ src/gateway/QuoteGateway.ts
@@ -5,7 +5,7 @@ const QuoteGateway = {
     const url = "https://api.adviceslip.com/advice";
     const response = await fetch(url, { cache: "no-cache" });
     const { slip } = await response.json();
-    return slip satisfies QuoteType;
+    return slip satisfies QuoteType as QuoteType;
   }
 };
 
